f15947ccaa5d114c6a68cd4430657cac20c96201
Kaden Weber <kweber142@gmail.com>
2020-07-08 12:47:09 -0500

    created v4.1 with job spaces

--- Supply/REDM/utils/data_prep.py
+++ Supply/REDM/utils/data_prep.py
@@ -1,6 +1,6 @@
 
 import pandas
-from utils.constants import REDM_IO_COLUMNS, MGRA, PRODUCT_TYPES
+from utils.constants import REDM_IO_COLUMNS, MGRA, NON_RESIDENTIAL_TYPES, job_spaces_columns
 from utils.interface import save_to_file
 
 '''
@@ -18,17 +18,37 @@ def load_interpolated():
     return sort_by_column(pandas.read_csv('data/interpolated_vars.csv'), MGRA)
 
 
-def add_job_units_columns(dataframe):
-    for product_type in PRODUCT_TYPES:
-        pass
-    pass
+def job_spaces_for_product_type(dataframe, product_type):
+    # get applicable columns
+    average_area_per_job_label, current_employment_label, total_area_label = \
+        job_spaces_columns(product_type)
+    average_area_per_job = dataframe[average_area_per_job_label]
+    current_employment = dataframe[current_employment_label]
+    total_area = dataframe[total_area_label]
+    # find estimate of unoccupied job spaces
+    used_area = average_area_per_job * current_employment
+    vacant_area = total_area - used_area
+    unoccupied_job_spaces = vacant_area / average_area_per_job
+    # if negative, assume that the spaces are all occupied
+    unoccupied_job_spaces[unoccupied_job_spaces < 0] = 0
+    total_job_spaces = current_employment + \
+        unoccupied_job_spaces.astype('int32')
+    return total_job_spaces
+
+
+def add_job_spaces_columns(dataframe):
+    for product_type in NON_RESIDENTIAL_TYPES:
+        dataframe[product_type +
+                  '_js'] = job_spaces_for_product_type(dataframe, product_type)
+    return dataframe
 
 
 def create_version_4point1():
     original_frame = pandas.read_csv("data/SRF_Input_Base_V4.csv")
     interpolated_frame = load_interpolated()
     combined_frame = pandas.merge(original_frame, interpolated_frame, on=MGRA)
-    final_frame = combined_frame[REDM_IO_COLUMNS]
+    frame_with_job_spaces = add_job_spaces_columns(combined_frame)
+    final_frame = frame_with_job_spaces[REDM_IO_COLUMNS]
     save_to_file(final_frame, 'data', 'SRF_Input_Base_V4.1.csv')
     return
 
