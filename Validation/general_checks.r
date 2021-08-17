981abb0f20aafec462e58c6af06e225c22c97dc0
Colby Brown <colby@manhangroup.com>
2021-08-17 14:31:15 -0400

    Update general_checks.r
    Do comparisons at TAZ level

--- Validation/general_checks.r
+++ Validation/general_checks.r
@@ -6,10 +6,21 @@ processed_out <- read.csv(file.path("..","PostProcessor","Data","mgra13_based_in
 library(dplyr)
 MGRA_comparison <- inner_join(MGRA_target,
                               processed_out,
-                              by=c("mgra"="mgra"),
+                              by=c("mgra"="mgra","taz"="taz","zip09"="zip09","luz_id"="luz_id"),
                               suffix=c(".obs",".est"))
 MGRA_comparison$hh.err = MGRA_comparison$hh.est - MGRA_comparison$hh.obs
 MGRA_comparison$emp.err = MGRA_comparison$emp_total.est - MGRA_comparison$emp_total.obs
-hh_WAAPE = sum(abs(MGRA_comparison$hh.err))/sum(MGRA_comparison$hh.obs)
-emp_WAAPE = sum(abs(MGRA_comparison$emp.err))/sum(MGRA_comparison$emp_total.obs)
+hh_WAAPE_mgra = sum(abs(MGRA_comparison$hh.err))/sum(MGRA_comparison$hh.obs)
+emp_WAAPE_mgra = sum(abs(MGRA_comparison$emp.err))/sum(MGRA_comparison$emp_total.obs)
 write.csv(MGRA_comparison,file.path("results","MGRA_comparison.csv"),row.names = FALSE)
+TAZ_comparison <- MGRA_comparison %>%
+  group_by(taz) %>%
+  summarise(hh.est = sum(hh.est),
+            hh.obs = sum(hh.obs),
+            hh.err = sum(hh.err),
+            emp.est = sum(emp_total.est),
+            emp.obs = sum(emp_total.obs),
+            emp.err = sum(emp.err))
+write.csv(TAZ_comparison,file.path("results","TAZ_comparison.csv"),row.names = FALSE)
+hh_WAAPE_TAZ = sum(abs(TAZ_comparison$hh.err))/sum(TAZ_comparison$hh.obs)
+emp_WAAPE_TAZ = sum(abs(TAZ_comparison$emp.err))/sum(TAZ_comparison$emp.obs)
