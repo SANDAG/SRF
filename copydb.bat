98eccbea4e125d838a58907b4c4f1a1911c0859a
Colby Brown <colby@manhangroup.com>
2021-12-02 18:48:26 +0000

    Changes to convenience scripts

--- copydb.bat
+++ copydb.bat
@@ -1,12 +1,11 @@
 REM This batch file can be used to create a copy of an SRF System database as a starting point for a new SRF scenario
-REM Arguments:
-REM 1. PostgreSQL username
-REM 2. PostgreSQL password
-REM 3. Source database name
-REM 4. Target database name
-REM 5. Temporary working folder
-set PGUSER=%1
-set PGPASSWORD=%2
-pg_dump -Fd -j 4 -d %3 -f %5/%3
-createdb %4
-pg_restore -Fd -j 4 -d $4 %5/%3
+ECHO OFF
+set /p PGUSER="PostgreSQL username: "
+set /p PGPASSWORD="PostgreSQL password: "
+set /p source_db="Source database name: "
+set /p target_db="Target database name: "
+ECHO ON
+ECHO Performing copy operation...
+pg_dump -Fd -j 4 -d %source_db% -f %TMP%/%source_db%
+createdb %target_db%
+pg_restore -Fd -j 4 -d %target_db% %TEMP%/%source_db%
--- restoredb.bat
+++ restoredb.bat
@@ -1,12 +1,12 @@
 REM This batch file can be used to restore an SRF System database from backup (i.e. during installation)
-REM Arguments:
-REM 1. PostgreSQL username
-REM 2. PostgreSQL password
-REM 3. globals.sql path
-REM 4. new db name
-REM 5. database backup dir path
-set PGUSER=%1
-set PGPASSWORD=%2
-psql -f %3
-createdb %4
-pg_restore -Fd -j 4 -d $4 %5
+ECHO OFF
+SET /p PGUSER="Please enter PostgreSQL username: "
+set /p PGPASSWORD="Please enter PostgreSQL password: "
+SET /p globals = "Please specify path to globals.sql file: "
+ECHO Restoring PostgreSQL globals...
+psql -f %globals%
+SET /p new_db = "Please specify new database name"
+SET /p backup_dir = "Please specify path to source database backup directory: "
+ECHO Creating new database and restoring from backup...
+createdb %new_db%
+pg_restore -Fd -j 4 -d %new_db% %backup_dir%
