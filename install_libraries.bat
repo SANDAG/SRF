d16872259b7c5dc3bb45783968151fd01fa14a1a
Colby Brown <colby@manhangroup.com>
2021-12-04 10:37:54 +0000

    Fixes to batch scripts
    Miscellaneous corrections

--- install_libraries.bat
+++ install_libraries.bat
@@ -1,7 +1,7 @@
 REM This script is written to streamline installation of required software libraries for the SRF System
 ECHO "Warning!  You must have already installed Anaconda Python and R before running this script!"
 proceed = "N"
-SET /p proceed=["Have you done so? (y/N):"]
+SET /p proceed="Have you done so? (y/N):"
 IF proceed EQU "N" CANCEL
 conda install psycopg2
 pip install pipenv
@@ -9,3 +9,8 @@ cd Supply
 pipenv install
 cd ..
 rscript Demand\install_requirements.R
+ECHO "Installation of required libraries complete."
+SET PATH=%PATH%;%ProgramFiles%\PostgreSQL
+ECHO "PostgreSQL added to path variable"
+SET /p mupath="Please specify location of mu-land.exe:"
+SET PATH=%PATH%;%mupath%
