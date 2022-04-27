8d65ecd1f4e80a17c6fb772a68b6ca38e34a55b4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 10:55:41 +0800

    Delete Supply directory

--- Supply/test_with_coverage.sh
+++
@@ -1,2 +0,0 @@
-coverage run --source=. --omit='playgrounds*' run_tests.py -t -o
-coverage report -m
--- Supply/utils/converter.py
+++
@@ -1,9 +0,0 @@
-SQUARE_FEET_PER_ACRE = 43560
-
-
-def square_feet_to_acres(square_feet):
-    return square_feet / SQUARE_FEET_PER_ACRE
-
-
-def x_per_acre_to_x_per_square_foot(x):
-    return x / SQUARE_FEET_PER_ACRE
